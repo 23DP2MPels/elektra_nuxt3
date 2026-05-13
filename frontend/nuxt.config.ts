46bc9a35817ba76dcbcd67a7316f4a22905872db
Mairis Pelšs <mairis.pelss@gmail.com>
2026-05-13 15:14:49 +0300

    fixed problem not showing website name and custom logo

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -31,7 +31,14 @@ export default defineNuxtConfig({
 
   app: {
     head: {
+      title: 'Elektra: Price Comparison',
+      titleTemplate: '%s | Elektra',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
       link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
         { rel: 'manifest', href: '/manifest.webmanifest' }
       ]
     }
